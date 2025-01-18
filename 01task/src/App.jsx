1e9430f7d72babba8ce05b7f5a3bf74397307495
Ranjan Kumar <mrranjankumarbxr@gmail.com>
2025-01-19 02:03:05 +0530

    task complete

--- 01task/src/App.jsx
+++ 01task/src/App.jsx
@@ -1,23 +1,20 @@
-function App() {
+
+function App(){
   return (
-    <>
-       <div className="flex items-center bg-white h-full">
-          <div className="w-1/2 pl-14">
-          <img src="https://www.phonepe.com/webstatic/8541/static/00ce8c2779b8e7913afa54fac6e2ffd4/8b553/1in3-desktop.png" alt="" />
-          </div>
+    <div className="flex flex-col md:flex-row items-center bg-white ">
+      <div className="flex flex-col items-center md:items-start" style={{height:"50%",width:"50%"}}>
+        <img src="https://www.phonepe.com/webstatic/8541/static/00ce8c2779b8e7913afa54fac6e2ffd4/8b553/1in3-desktop.png" alt="" />
+      </div>
 
+      <div className="mt-6 md:mt-0 md:ml-10">
+        <p className=" text-violet-800 leading-12 text-center md:text-left font-sans text-3xl">
+          That’s why we have an advanced
+          <p>security infrastructure to keep your</p> <p>transactions safe. We do what it</p><p>takes to earn the trust that you and</p> 58.5+ crore Indians have placed in us.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-          <div className="mt-6 md:mt-0 md:ml-10 text-center md:text-left max-w-md w-1/2 pl-14">
-               <p className=" text-xl text-violet-800 font-sans tracking-widest">
-                    That’s why we have an advanced security infrastructure to keep your 
-                    transactions safe. We do what it
-                    takes to earn the trust that you and
-                    <p> 58.5+ crore Indians have placed in us.</p>
-               </p>
-          </div>
-        </div>
-    </>
-  )
-}
+export default App;
 
-export default App
