80d1b332995711f222670da64d12df2ac54855b1
Ranjan Kumar <mrranjankumarbxr@gmail.com>
2025-01-19 01:23:32 +0530

    task complete

---
+++ 01task/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+       <div className="flex items-center bg-white h-full">
+          <div className="w-1/2 pl-14">
+          <img src="https://www.phonepe.com/webstatic/8541/static/00ce8c2779b8e7913afa54fac6e2ffd4/8b553/1in3-desktop.png" alt="" />
+          </div>
+
+
+          <div className="mt-6 md:mt-0 md:ml-10 text-center md:text-left max-w-md w-1/2 pl-14">
+               <p className=" text-xl text-violet-800 font-sans tracking-widest">
+                    That’s why we have an advanced security infrastructure to keep your 
+                    transactions safe. We do what it
+                    takes to earn the trust that you and
+                    <p> 58.5+ crore Indians have placed in us.</p>
+               </p>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default App
