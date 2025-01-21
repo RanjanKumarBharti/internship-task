719e0ab7b470da54db532cdfa49c51d99599b6cd
Ranjan Kumar <mrranjankumarbxr@gmail.com>
2025-01-21 19:16:18 +0530

    processing

---
+++ 01task/src/components/Task2.jsx
@@ -0,0 +1,9 @@
+function Task2() {
+  return (
+    <div className="flex justify-center items-center bg-gradient-to-r from-purple-400 to-blue-400 p-4 h-96">
+      <div className="max-w-4xl w-full">
+      </div>
+    </div>
+  );
+};
+export default Task2;
