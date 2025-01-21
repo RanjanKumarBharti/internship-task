5f780c784ccaeb6bdc8f67746e979b90c6a7d711
Ranjan Kumar <mrranjankumarbxr@gmail.com>
2025-01-21 23:54:03 +0530

    Processing

--- 01task/src/App.jsx
+++ 01task/src/App.jsx
@@ -1,13 +1,16 @@
 // import Task1 from "./components/task1";
-import Task2 from "./components/Task2";
+
+import Task3 from "./components/Task3";
+
+// import Task2 from "./components/Task2";
 function App(){
   return (
      <>
-       <Task2/>
+       {/* <Task2/> */}
        {/* <Task1/> */}
+       <Task3/>
      </>
   );
 };
 
 export default App;
-
